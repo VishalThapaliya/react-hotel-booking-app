0244887e702b2e4f35c356b6f9253167c61a0c73
VishalThapaliya <vishal.thapaliya@gmail.com>
2025-05-02 12:08:03 +0200

    add navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="nav-container">
+            <span className="logo">BibiBooking</span>
+
+            <div className="nav-items">
+                <button className="nav-button">Register</button>
+                <button className="nav-button">Login</button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
