47ced82d66d8d17d038d478a08e1d7eb587e39cb
Bishal Thapaliya <vishal.thapaliya@gmail.com>
2025-05-07 16:47:23 +0200

    add featured components and footer to complete home page (#4)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import Featured from '../../components/featured/Featured'
+import './Home.css';
+import PropertyList from '../../components/propertyList/PropertyList';
+import HomeGuests from '../../components/homeGuests/HomeGuests';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
 
 const Home = () => {
   return (
-    <section>
+    <>
       <Header />
-    </section>
+      <main className="main-container">
+        <Featured />
+
+        <h2 className="home-title">Browse by property type</h2>
+        <PropertyList />
+
+        <h2 className="home-title">Accommodations that guests love</h2>
+        <HomeGuests />
+
+        <MailList />
+
+        <Footer />
+      </main>
+    </>
   )
 }
 
